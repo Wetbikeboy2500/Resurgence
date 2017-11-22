6546268423d8926a955d1a2f516cf06da519e403
Matt <wetbikeboy2500@gmail.com>
2017-11-22 11:27:03 -0500

    Update 7.5
    doesn't run in iframe

--- ScratchFixer.user.js
+++ ScratchFixer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ResurgenceUserscript
 // @namespace    http://tampermonkey.net/
-// @version      7.4
+// @version      7.5
 // @description  Tries to fix and improve certain aspects of Scratch
 // @author       Wetbikeboy2500
 // @match        https://scratch.mit.edu/*
@@ -16,55 +16,56 @@
 // ==/UserScript==
 (function () {
     'use strict';
-    window.addEventListener("load", () => {
-        if (ran_code == false) {
-            console.log("window loaded");
-            ran_code = true;
-            run();
-        }
-    }, false);
-    //make sure code runs if window loading dosn't work
-    setTimeout (() => {
-        if (ran_code == false) {
-            console.log("load interval");
-            ran_code = true;
-            run();
-        }
-    },5000);//5 second wait for the page
+    if (inIframe() == false) {
+        window.addEventListener("load", () => {
+            if (ran_code == false) {
+                console.log("window loaded");
+                ran_code = true;
+                run();
+            }
+        }, false);
+        //make sure code runs if window loading dosn't work
+        setTimeout (() => {
+            if (ran_code == false) {
+                console.log("load interval");
+                ran_code = true;
+                run();
+            }
+        },5000);//5 second wait for the page
 
-    function run () {
-        load_userinfo();
-        if (url.includes("discuss")) {
-            load_images();
-            load_scratchblockcode();
-            load_bbcode();
-            add_bbtags();
+        function run () {
+            load_userinfo();
+            if (url.includes("discuss")) {
+                load_images();
+                load_scratchblockcode();
+                load_bbcode();
+                add_bbtags();
+            }
         }
+
+        let url = window.location.href, users = [], userinfo = {}, l, ran_code = false, style = null, style1 = null;
+        //adds my css to edit custom elements
+        if (GM_getValue("theme", false) === "dark") {
+            style1 = GM_addStyle(GM_getResourceText("CSS"));
+        } 
+        document.addEventListener("DOMContentLoaded", () => {
+            var styleTip ='.tips a span { display: none; position: absolute; } .tips a:after { content: "' + GM_getValue("forumTitle", "Forums") + '"; visibility: visible; position: static; } .phosphorus { margin-left: 14px; margin-right: 14px; margin-top: 16px; } .my_select {height: 34px; line-height: 34px; vertical-align: middle; margin: 3px 0px 3px 0px; width: 110px;} .messages-social {width: 700px; right: 446.5px; left: 235.5px; position: relative; border: 0.5px solid #F0F0F0; border-top-left-radius: 5px; border-top-right-radius: 5px; border-bottom-left-radius: 5px; border-bottom-right-radius: 5px; background-color: #F2F2F2; } .messages-header {font-size: 24px; padding-left: 10px;} select[name="messages.filter"] {right: 720px; top: 20px; font-size: 24px; position: relative; border-top-left-radius: 5px; border-top-right-radius: 5px; border-bottom-left-radius: 5px; border-bottom-right-radius: 5px; background-color: #F2F2F2; visibility: visible;} #___gcse_0 {display: none;} .messages-details {margin-top: 40px;} .mod-messages {visibility: hidden; height: 0px; padding: 0px; margin: 0px;}';
+            GM_addStyle(styleTip);
+            dark_theme();
+            fix_nav();
+            load_newpage();
+            add_player();
+            add_search();
+            load_extras();
+            load_messages();
+        });
     }
-
-    let url = window.location.href, users = [], userinfo = {}, l, ran_code = false, style = null, style1 = null;
-    //adds my css to edit custom elements
-    if (GM_getValue("theme", false) === "dark") {
-        style1 = GM_addStyle(GM_getResourceText("CSS"));
-    } 
-    document.addEventListener("DOMContentLoaded", () => {
-        var styleTip ='.tips a span { display: none; position: absolute; } .tips a:after { content: "' + GM_getValue("forumTitle", "Forums") + '"; visibility: visible; position: static; } .phosphorus { margin-left: 14px; margin-right: 14px; margin-top: 16px; } .my_select {height: 34px; line-height: 34px; vertical-align: middle; margin: 3px 0px 3px 0px; width: 110px;} .messages-social {width: 700px; right: 446.5px; left: 235.5px; position: relative; border: 0.5px solid #F0F0F0; border-top-left-radius: 5px; border-top-right-radius: 5px; border-bottom-left-radius: 5px; border-bottom-right-radius: 5px; background-color: #F2F2F2; } .messages-header {font-size: 24px; padding-left: 10px;} select[name="messages.filter"] {right: 720px; top: 20px; font-size: 24px; position: relative; border-top-left-radius: 5px; border-top-right-radius: 5px; border-bottom-left-radius: 5px; border-bottom-right-radius: 5px; background-color: #F2F2F2; visibility: visible;} #___gcse_0 {display: none;} .messages-details {margin-top: 40px;} .mod-messages {visibility: hidden; height: 0px; padding: 0px; margin: 0px;}';
-        GM_addStyle(styleTip);
-        dark_theme();
-        fix_nav();
-        load_newpage();
-        add_player();
-        add_search();
-        load_extras();
-        load_messages();
-    });
-
     function fix_nav () {
         //fixes navbar
         if (document.getElementById("navigation") !== null) {
             document.getElementsByClassName("tips")[0].childNodes[0].setAttribute("href", "/discuss");
             console.log("New Theme");
-        } else {
+        } else if (document.getElementById("site-nav") !== null) {
             let tips = document.getElementsByClassName("site-nav")[0].childNodes[3].childNodes[0];
             tips.setAttribute("href", "/discuss");
             tips.innerHTML = GM_getValue("forumTitle", "Forums");
@@ -991,13 +992,13 @@
     //add extras bbcode features
     function add_bbtags () {
         $('<li class="markItUpButton markItUpButtonRes1" id="Res1"><a  title="Color" >Color</a></li>').insertAfter(".markItUpButton7")
-        .find("a").css("background-image", "url(https://png.icons8.com/color-wheel/office/14/000000)");
+            .find("a").css("background-image", "url(https://png.icons8.com/color-wheel/office/14/000000)");
         $('<li class="markItUpButton markItUpButtonRes2" id="Res2"><a  title="Code" >Code</a></li>').insertAfter(".markItUpButton11")
-        .find("a").css("background-image", "url(https://png.icons8.com/code/office/16/000000)");
+            .find("a").css("background-image", "url(https://png.icons8.com/code/office/16/000000)");
         $('<li class="markItUpButton markItUpButtonRes2" id="Res3"><a  title="Center" >Center</a></li>').insertAfter(".markItUpButton4")
-        .find("a").css("background-image", "url(https://png.icons8.com/align-center/office/16/000000)");
+            .find("a").css("background-image", "url(https://png.icons8.com/align-center/office/16/000000)");
         $('<li class="markItUpButton markItUpButtonRes2" id="Res4"><a  title="Project link" >Project Link</a></li>').insertAfter(".markItUpButton14")
-        .find("a").css("background-image", "url(https://png.icons8.com/prototype/office/16/000000)");
+            .find("a").css("background-image", "url(https://png.icons8.com/prototype/office/16/000000)");
         document.onselectionchange = function() {
             document.stringyBB = getSelectionText();
         };
@@ -1044,6 +1045,13 @@
             newtxt+
             $(txtarea).val().substring(txtarea.selectionEnd)
         );
+    }
+    function inIframe () {
+        try {
+            return window.self !== window.top;
+        } catch (e) {
+            return true;
+        }
     }
     function element (name) {
         return new _element(name);
