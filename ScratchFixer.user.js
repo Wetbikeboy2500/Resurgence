a49e6d7b9a1f0c6ca8c11de5935afa5be7115b7f
Matt <wetbikeboy2500@gmail.com>
2018-12-17 17:30:16 -0500

    Working on theme fix

--- ScratchFixer.user.js
+++ ScratchFixer.user.js
@@ -319,16 +319,22 @@ SOFTWARE.
             //removes borders on boxes which I think looks better
             themeTweakStyle = GM_addStyle(".box {border: 0px;}");
 
-            //changes how projects are cycled through with them no longer using the same theme
-            fetch("https://api.scratch.mit.edu/proxy/featured")
-                .then((response) => response.json())
-                .then((json) => {
-                    //this will process the different categories that will be displayed on the mian page of scratch
-
-                })
-                .catch((e) => {
-                    console.alert("An error occured in theme tweaks in fetch", e);
-                });
+            if (url == "https://scratch.mit.edu/") {
+                //changes how projects are cycled through with them no longer using the same theme
+                fetch("https://api.scratch.mit.edu/proxy/featured")
+                    .then((response) => response.json())
+                    .then((json) => {
+                        //this will process the different categories that will be displayed on the main page of scratch
+                        console.log(json["community_featured_projects"]);
+                        element("div").a("class", "box")
+                            .add("div").a("class", "box-header").addDom(svg("leftCircle", { "style": "float: left;" })).add("h4").t("Featured Projects").a("style", "padding: 1.5px 10px 0px 10px;").f().addDom(svg("rightCircle")).f()
+                            .add("div").a("class", "box-content").t("Hello world").f()
+                            .apAfter(".splash-header");
+                    })
+                    .catch((e) => {
+                        console.warn("An error occured in theme tweaks in fetch", e);
+                    });
+            }
 
         } else if (themeTweakStyle) {
             themeTweakStyle.parentElement.removeChild(themeTweakStyle);
@@ -2043,14 +2049,24 @@ SOFTWARE.
         return height;
     }
     //this function is for svg elements
-    function svg (name) {
+    function svg (name, attr) {
         const svgMap = new Map([
             ["write", `<svg class="svg-icon" viewBox="0 0 20 20"><path d="M18.303,4.742l-1.454-1.455c-0.171-0.171-0.475-0.171-0.646,0l-3.061,3.064H2.019c-0.251,0-0.457,0.205-0.457,0.456v9.578c0,0.251,0.206,0.456,0.457,0.456h13.683c0.252,0,0.457-0.205,0.457-0.456V7.533l2.144-2.146C18.481,5.208,18.483,4.917,18.303,4.742 M15.258,15.929H2.476V7.263h9.754L9.695,9.792c-0.057,0.057-0.101,0.13-0.119,0.212L9.18,11.36h-3.98c-0.251,0-0.457,0.205-0.457,0.456c0,0.253,0.205,0.456,0.457,0.456h4.336c0.023,0,0.899,0.02,1.498-0.127c0.312-0.077,0.55-0.137,0.55-0.137c0.08-0.018,0.155-0.059,0.212-0.118l3.463-3.443V15.929z M11.241,11.156l-1.078,0.267l0.267-1.076l6.097-6.091l0.808,0.808L11.241,11.156z"></path></svg>`]
             , ["downCircle", `<svg class="svg-icon" viewBox="0 0 20 20"><path d="M13.962,8.885l-3.736,3.739c-0.086,0.086-0.201,0.13-0.314,0.13S9.686,12.71,9.6,12.624l-3.562-3.56C5.863,8.892,5.863,8.611,6.036,8.438c0.175-0.173,0.454-0.173,0.626,0l3.25,3.247l3.426-3.424c0.173-0.172,0.451-0.172,0.624,0C14.137,8.434,14.137,8.712,13.962,8.885 M18.406,10c0,4.644-3.763,8.406-8.406,8.406S1.594,14.644,1.594,10S5.356,1.594,10,1.594S18.406,5.356,18.406,10 M17.521,10c0-4.148-3.373-7.521-7.521-7.521c-4.148,0-7.521,3.374-7.521,7.521c0,4.147,3.374,7.521,7.521,7.521C14.148,17.521,17.521,14.147,17.521,10"></path></svg>`]
             , ["upCircle", `<svg class="svg-icon" viewBox="0 0 20 20"><path d="M13.889,11.611c-0.17,0.17-0.443,0.17-0.612,0l-3.189-3.187l-3.363,3.36c-0.171,0.171-0.441,0.171-0.612,0c-0.172-0.169-0.172-0.443,0-0.611l3.667-3.669c0.17-0.17,0.445-0.172,0.614,0l3.496,3.493C14.058,11.167,14.061,11.443,13.889,11.611 M18.25,10c0,4.558-3.693,8.25-8.25,8.25c-4.557,0-8.25-3.692-8.25-8.25c0-4.557,3.693-8.25,8.25-8.25C14.557,1.75,18.25,5.443,18.25,10 M17.383,10c0-4.07-3.312-7.382-7.383-7.382S2.618,5.93,2.618,10S5.93,17.381,10,17.381S17.383,14.07,17.383,10"></path></svg>`]
+            , ["rightCircle", `<svg class="svg-icon" viewBox="0 0 20 20"><path d="M12.522,10.4l-3.559,3.562c-0.172,0.173-0.451,0.176-0.625,0c-0.173-0.173-0.173-0.451,0-0.624l3.248-3.25L8.161,6.662c-0.173-0.173-0.173-0.452,0-0.624c0.172-0.175,0.451-0.175,0.624,0l3.738,3.736C12.695,9.947,12.695,10.228,12.522,10.4 M18.406,10c0,4.644-3.764,8.406-8.406,8.406c-4.644,0-8.406-3.763-8.406-8.406S5.356,1.594,10,1.594C14.643,1.594,18.406,5.356,18.406,10M17.521,10c0-4.148-3.374-7.521-7.521-7.521c-4.148,0-7.521,3.374-7.521,7.521c0,4.147,3.374,7.521,7.521,7.521C14.147,17.521,17.521,14.147,17.521,10"></path></svg>`]
+            , ["leftCircle", `<svg class="svg-icon" viewBox="0 0 20 20"><path d="M11.739,13.962c-0.087,0.086-0.199,0.131-0.312,0.131c-0.112,0-0.226-0.045-0.312-0.131l-3.738-3.736c-0.173-0.173-0.173-0.454,0-0.626l3.559-3.562c0.173-0.175,0.454-0.173,0.626,0c0.173,0.172,0.173,0.451,0,0.624l-3.248,3.25l3.425,3.426C11.911,13.511,11.911,13.789,11.739,13.962 M18.406,10c0,4.644-3.763,8.406-8.406,8.406S1.594,14.644,1.594,10S5.356,1.594,10,1.594S18.406,5.356,18.406,10 M17.521,10c0-4.148-3.373-7.521-7.521-7.521c-4.148,0-7.521,3.374-7.521,7.521c0,4.148,3.374,7.521,7.521,7.521C14.147,17.521,17.521,14.148,17.521,10"></path></svg>`]
         ]);
 
-        return document.createRange().createContextualFragment(svgMap.get(name)).firstChild;
+        let dom = document.createRange().createContextualFragment(svgMap.get(name)).firstChild;
+
+        if (attr && attr.constructor === {}.constructor) {
+            for (let a in attr) {
+                dom.setAttribute(a, attr[a]);
+            }
+        }
+
+        return dom;
     }
 
     //the following is my own custom dom creation object that I continue to improve as I use it
